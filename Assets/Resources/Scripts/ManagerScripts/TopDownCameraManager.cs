9679f977341f5f61e31a8c10212e974812a96ed2
s0578026 <kay.birth@student.htw-berlin.de>
2022-06-30 14:38:32 +0200

    added stones and fixed camera settings

--- Assets/Resources/Scripts/ManagerScripts/TopDownCameraManager.cs
+++ Assets/Resources/Scripts/ManagerScripts/TopDownCameraManager.cs
@@ -19,22 +19,22 @@ namespace CGGame.Cameras
         private float m_Angle = 45f;
 
         [SerializeField]
-        private float m_SmoothSpeed = 0.8f;
+        private float m_MovmentSpeed = 40f;
 
         [SerializeField]
-        private float m_MovmentSpeed = 20f;
+        private float m_RotatingSpeed = 30f;
 
         [SerializeField]
-        private float minX = -300f;
+        private float minX = -150f;
 
         [SerializeField]
-        private float minZ = -300f;
+        private float minZ = -150f;
 
         [SerializeField]
-        private float maxX = 300f;
+        private float maxX = 1500f;
 
         [SerializeField]
-        private float maxZ = 300f;
+        private float maxZ = 150f;
 
         private Vector3 refVelocity;
         private float inputX;
@@ -56,20 +56,20 @@ namespace CGGame.Cameras
                 inputX = Input.GetAxis("Horizontal");
                 inputZ = Input.GetAxis("Vertical");
 
-                m_Distance = Mathf.Clamp(Mathf.Abs(m_Distance + (Input.mouseScrollDelta.y*2)) ,5 ,50);
+                m_Distance = Mathf.Clamp(Mathf.Abs(m_Distance + (Input.mouseScrollDelta.y*2)) ,5 ,20);
                 m_Hight = Mathf.Pow(1.2f*m_Distance,1.1f);
 
-                m_Target.Translate(new Vector3(inputX,5,inputZ)*Time.deltaTime*m_MovmentSpeed);
+                m_Target.Translate(new Vector3(inputX,7,inputZ)*Time.deltaTime*m_MovmentSpeed);
 
-                m_Target.SetPositionAndRotation(new Vector3(Mathf.Clamp(m_Target.position.x,minX,maxX),5,Mathf.Clamp(m_Target.position.z,minZ,maxZ)),Quaternion.identity);
+                m_Target.SetPositionAndRotation(new Vector3(Mathf.Clamp(m_Target.position.x,minX,maxX),7,Mathf.Clamp(m_Target.position.z,minZ,maxZ)),Quaternion.identity);
                 
                 if (Input.GetKey(KeyCode.R))
                 {
-                    m_Angle = (m_Angle+0.1f) % 381;
+                    m_Angle = (m_Angle+(m_RotatingSpeed*Time.deltaTime)) % 381;
                 }
                 if (Input.GetKey(KeyCode.F))
                 {
-                    m_Angle = (m_Angle-0.1f) % 381;
+                    m_Angle = (m_Angle-(m_RotatingSpeed*Time.deltaTime)) % 381;
                 }
                 
                 Vector3 worldPosition = (Vector3.forward * -m_Distance) + (Vector3.up * m_Hight);
